9548d60f8c6637c732e9f748790aaed8bfa3ec1d
KEY <keyfunc@gmail.com>
2014-10-06 23:18:39 +0800

    修复新版下2P模式失效问题

--- assets/js/heikeji.src.js
+++ assets/js/heikeji.src.js
@@ -1,5 +1,5 @@
 /*!
- * bilibili_hkj v0.2.7 (http://keyfunc.github.io/bilibili_hkj)
+ * bilibili_hkj v0.2.8 (http://keyfunc.github.io/bilibili_hkj)
  * Copyright 2012-2014 Key Dai
  * Licensed under MIT (http://github.com/keyfunc/bilibili_hkj/blob/master/LICENSE)
  */
@@ -64,7 +64,7 @@
     head = d.getElementsByTagName("head")[0];
     style = d.createElement("style");
     style.type = "text/css";
-    style.appendChild(d.createTextNode("#_callouts {\n  position: fixed;\n  right: 16px;\n  bottom: 32px;\n  z-index: 100;\n}\n#_callouts div {\n  position: relative;\n  margin: 10px 0;\n  padding: 7px 15px 7px 12px;\n  text-align:left;\n  border-left:5px solid #eee;\n  font: 13px/1.65 \"Segoe UI\", \"Helvetica Neue\", Helvetica, Arial, \"Hiragino Kaku Gothic Pro\", \"Meiryo\", \"Hiragino Sans GB\", \"Microsoft YaHei\", \"STHeiti\", \"SimSun\", Sans-serif;\n  z-index: 100;\n}\n#_callouts div p,\n#_callouts div h4 {\n  margin: 0;\n}\n#_callouts div.message {\n  background-color: #fdf7f7;\n  border-color: #d9534f;\n}\n#_callouts div.message h4,\n#_callouts div.message h4 * {\n  color: #d9534f;\n  text-decoration: none;\n}\n#_callouts div.tip {\n  background-color: #f4f8fa;\n  border-color: #5bc0de;\n}"));
+    style.appendChild(d.createTextNode("#_callouts {\n  position: fixed;\n  right: 16px;\n  bottom: 32px;\n  z-index: 999;\n}\n#_callouts div {\n  position: relative;\n  margin: 10px 0;\n  padding: 7px 15px 7px 12px;\n  text-align:left;\n  border-left:5px solid #eee;\n  font: 13px/1.65 \"Segoe UI\", \"Helvetica Neue\", Helvetica, Arial, \"Hiragino Kaku Gothic Pro\", \"Meiryo\", \"Hiragino Sans GB\", \"Microsoft YaHei\", \"STHeiti\", \"SimSun\", Sans-serif;\n  z-index: 999;\n}\n#_callouts div p,\n#_callouts div h4 {\n  margin: 0;\n}\n#_callouts div.message {\n  background-color: #fdf7f7;\n  border-color: #d9534f;\n}\n#_callouts div.message h4,\n#_callouts div.message h4 * {\n  color: #d9534f;\n  text-decoration: none;\n}\n#_callouts div.tip {\n  background-color: #f4f8fa;\n  border-color: #5bc0de;\n}"));
     head.appendChild(style);
     callouts = d.createElement("div");
     callouts.id = "_callouts";
@@ -75,9 +75,9 @@
       })();
       return;
     }
-    messageExpires = (new Date("2014/08/23")).getTime() + 15 * 1000 * 3600 * 24;
+    messageExpires = (new Date("2014/10/6")).getTime() + 15 * 1000 * 3600 * 24;
     if (new Date().getTime() - messageExpires < 0) {
-      _c_message(callouts, "修复了2P模式失效的问题", "http://keyfunc.github.io/bilibili_hkj/", "修复“用手机看”无法正常显示问题");
+      _c_message(callouts, "修复新版bilibili下2P模式失效问题", "http://keyfunc.github.io/bilibili_hkj/", "重构部分代码，增加异常处理");
     }
     jQueryVersion = "1.11.1";
     this.Loader = {
@@ -93,6 +93,20 @@
         };
         script.src = url;
         return head.appendChild(script);
+      },
+      importCSS: function(url, head, func) {
+        var css;
+        css = d.createElement("link");
+        css.type = "text/css";
+        css.rel = "stylesheet";
+        css.onload = function() {
+          console.log("onLoad: " + url);
+          if (func !== void 0) {
+            return func();
+          }
+        };
+        css.href = url;
+        return head.appendChild(css);
       }
     };
     loadjQuery = true;
@@ -117,11 +131,11 @@
       }
     }
     normalModel = function(_params) {
-      var api, bofqi, jsonp, url;
+      var api, jsonp, url;
       api = "http://api.bilibili.com/view?type=json&appkey=12737ff7776f1ade&id=" + _params[2] + (_params[3] !== void 0 ? "&page=" + _params[3] : "");
       url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url=%22" + encodeURIComponent(api) + "%22&format=json&callback=cbfunc";
-      bofqi = d.getElementById("bofqi");
-      bofqi.innerHTML = "";
+      $("#bofqi").empty();
+      $("#bofqi").append("<div id='player_placeholder' class='player'></div>");
       _c_tip(callouts, "成功干掉原先的播放器");
       jsonp = d.createElement("script");
       jsonp.setAttribute("src", url);
@@ -145,28 +159,36 @@
           $z = $("<div class=\"z\"></div>");
           $(".header").after($z);
           return $z.load(_bilibili_url + "/video/av877489/ .z:first", function() {
-            $(".cover_image", $z).hide();
-            $(".tminfo span[typeof='v:Breadcrumb']", $z).remove();
-            $(".tminfo time", $z).remove();
-            $("#assdown", $z).attr("href", $("#assdown", $z).attr("href").replace("877489", _params[2]));
-            $(".info .sf a:last", $z).attr("href", $(".info .sf a:last", $z).attr("href").replace("877489", _params[2]));
-            $(".info .sf a:last", $z).attr("onclick", $(".info .sf a:last", $z).attr("onclick").replace("877489", _params[2]));
-            $(".ad-f", $z).remove();
-            $(".ad-e1", $z).remove();
-            $(".ad-e2", $z).remove();
-            $(".ad-e3", $z).remove();
-            $(".ad-e4", $z).remove();
-            $(".info h2", $z).text("").attr("title", "");
-            $(".intro", $z).text("");
-            $(".common .comm img", $z).attr("onclick", $(".common .comm img", $z).attr("onclick").replace("877489", _params[2]));
-            $("#newtag a", $z).attr("onclick", $("#newtag a", $z).attr("onclick").replace("877489", _params[2]));
-            $("#recommend_frmPost input[name='aid']", $z).val(_params[2]);
+            var error;
+            try {
+              $(".cover_image", $z).hide();
+              $(".tminfo span[typeof='v:Breadcrumb']", $z).remove();
+              $(".tminfo time", $z).remove();
+              $(".info .fav_btn_top", $z).attr("onclick", $(".info .fav_btn_top", $z).attr("onclick").replace("877489", _params[2]));
+              $(".arc-tool-bar .fav a", $z).attr("href", $(".arc-tool-bar .fav a", $z).attr("href").replace("877489", _params[2]));
+              $(".arc-tool-bar .fav a", $z).attr("onclick", $(".arc-tool-bar .fav a", $z).attr("onclick").replace("877489", _params[2]));
+              $(".ad-f", $z).remove();
+              $(".ad-e1", $z).remove();
+              $(".ad-e2", $z).remove();
+              $(".info h2", $z).text("").attr("title", "");
+              $(".intro", $z).text("");
+              $(".common .comm img", $z).attr("onclick", $(".common .comm img", $z).attr("onclick").replace("877489", _params[2]));
+              $("#newtag a", $z).attr("onclick", $("#newtag a", $z).attr("onclick").replace("877489", _params[2]));
+              $("#rate_frm", $z).attr("aid", _params[2]);
+            } catch (_error) {
+              error = _error;
+              console.log(error);
+            }
             if (w.history.pushState !== void 0) {
               w.history.pushState(null, _title, _bilibili_url + "/video/av" + _params[2]);
             }
             Loader.importJS('http://static.hdslb.com/js/page.arc.js', head);
             Loader.importJS('http://static.hdslb.com/js/jquery.qrcode.min.js', head);
             Loader.importJS('http://interface.bilibili.cn/count?aid=' + _params[2], head);
+            Loader.importJS('http://static.hdslb.com/js/swfobject.js', head);
+            Loader.importJS('http://static.hdslb.com/js/video.min.js', head);
+            Loader.importCSS('http://static.hdslb.com/css/bpoint/bpoints.css', head);
+            Loader.importJS('http://static.hdslb.com/js/bpoint/bpoints.js', head);
             return normalModel(_params);
           });
         });
@@ -177,7 +199,7 @@
           return _c_tip(callouts, "2P模式初始化完成", "＼(・ω・＼)丧尸！(／・ω・)／bishi");
         })();
       } else {
-        if (!!d.getElementById("bofqi_embed") || (function() {
+        if (!!d.getElementById("player_placeholder") || !!d.getElementById("bofqi_embed") || (function() {
           var iframePlay, iframes, key;
           iframes = d.getElementsByTagName("iframe");
           iframePlay = /https:\/\/secure\.bilibili\.com\/secure,cid=([0-9]+)(?:&aid=([0-9]+))?/;
@@ -197,7 +219,7 @@
       }
     };
     this.cbfunc = function() {
-      var iframe, info, onMessage;
+      var info;
       info = arguments[0].query.results.json;
       if (info.cid !== void 0) {
         _c_tip(callouts, "获取神秘代码成功", "神秘代码ID: " + info.cid);
@@ -206,37 +228,9 @@
           $(".intro").text(info.description);
           this.mid = info.mid;
           kwtags(info.tag.split(","), "");
+          $("title").text(info.title);
         }
-        iframe = d.createElement("iframe");
-        iframe.height = 482;
-        iframe.width = 950;
-        iframe.src = "https://secure.bilibili.com/secure,cid=" + info.cid + "&amp;aid=" + _params[2];
-        iframe.setAttribute("class", "player");
-        iframe.setAttribute("border", 0);
-        iframe.setAttribute("scrolling", "no");
-        iframe.setAttribute("frameborder", "no");
-        iframe.setAttribute("framespacing", 0);
-        bofqi.appendChild(iframe);
-        if (w.postMessage) {
-          onMessage = function(e) {
-            if (e.origin === "https://secure.bilibili.com" && e.data.substr(0, 6) === "secJS:") {
-              return eval(e.data.substr(6));
-            }
-          };
-          if (w.addEventListener) {
-            w.addEventListener("message", onMessage, false);
-          } else if (w.attachEvent) {
-            w.attachEvent("onmessage", onMessage);
-          }
-        } else {
-          setInterval(function() {
-            var evalCode;
-            if (evalCode = __GetCookie('__secureJS')) {
-              __SetCookie('__secureJS', '');
-              return eval(evalCode);
-            }
-          }, 1000);
-        }
+        EmbedPlayer('player', "http://static.hdslb.com/play.swf", "cid=" + info.cid + "&aid=" + _params[2]);
         return (w.bilibili_hkj = function() {
           return _c_tip(callouts, "Mission Completed", "嗶哩嗶哩 - ( ゜- ゜)つロ  乾杯~");
         })();
